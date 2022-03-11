b7ae0442c19a29933ea90cc84ac649b3201db989
Sarinsa <mikkemusbruker@gmail.com>
2022-03-11 01:32:02 +0100

    Basic project structure (wow so cool)

---
+++ src/main/java/com/sarinsa/starvingtotem/common/core/StarvingTotem.java
@@ -0,0 +1,18 @@
+package com.sarinsa.starvingtotem.common.core;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(StarvingTotem.MODID)
+public class StarvingTotem {
+
+    public static final String MODID = "starvingtotem";
+
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+
+    public StarvingTotem() {
+
+    }
+}
