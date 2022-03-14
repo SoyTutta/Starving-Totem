3414eebd149f7b2ea9783501aac37e8372596e4b
Sarinsa <mikkemusbruker@gmail.com>
2022-03-14 02:06:21 +0100

    Downgraded ForgeGradle + AaAAUUGHHH

--- src/main/java/com/sarinsa/starvingtotem/common/hooks/mixin/CommonMixinHooks.java
+++ src/main/java/com/sarinsa/starvingtotem/common/hooks/mixin/CommonMixinHooks.java
@@ -4,11 +4,22 @@ import com.sarinsa.starvingtotem.common.core.registry.STEffects;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Random;
+
 public class CommonMixinHooks {
 
     public static void checkTotemDeathProtection(LivingEntity livingEntity, CallbackInfoReturnable<Boolean> ci) {
         if (livingEntity.hasEffect(STEffects.BITTER_CURSE.get())) {
             ci.setReturnValue(false);
         }
+    }
+
+    public static int getBlessingStackSize(LivingEntity entity, Random random, int originalSize) {
+        if (entity.hasEffect(STEffects.SWEET_BLESSING.get())) {
+            if (random.nextInt(4) == 0) {
+                return 0;
+            }
+        }
+        return originalSize;
     }
 }
--- src/main/java/com/sarinsa/starvingtotem/common/mixin/LivingEntityMixin.java
+++ src/main/java/com/sarinsa/starvingtotem/common/mixin/LivingEntityMixin.java
@@ -4,11 +4,14 @@ import com.sarinsa.starvingtotem.common.hooks.mixin.CommonMixinHooks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.Effect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
@@ -18,8 +21,13 @@ public abstract class LivingEntityMixin extends Entity {
         super(entityType, world);
     }
 
-    @Inject(method = "checkTotemDeathProtection", at = @At("HEAD"))
+    @Inject(method = "checkTotemDeathProtection", at = @At("HEAD"), cancellable = true)
     public void onCheckTotemDeathProtection(DamageSource damageSource, CallbackInfoReturnable<Boolean> ci) {
         CommonMixinHooks.checkTotemDeathProtection((LivingEntity) (Object) this, ci);
+    }
+
+    @ModifyArg(method = "checkTotemDeathProtection", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;shrink(I)V"), index = 0)
+    public int modifyCheckTotemDeathProtection(int i) {
+        return CommonMixinHooks.getBlessingStackSize((LivingEntity) (Object) this, this.random, i);
     }
 }
